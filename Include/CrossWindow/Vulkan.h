5d60b52024660b912600db01a5402ba6c1064464
Siddharth Mishra <admin@brightprogrammer.in>
2024-04-09 21:04:31 +0530

    Added LICENSE

--- Include/CrossWindow/Vulkan.h
+++ Include/CrossWindow/Vulkan.h
@@ -1,18 +1,40 @@
 /**
- * @file Window.h 
+ * @file Vulkan.h 
  * @time 07/04/2024 23:05:25
  * @author Siddharth Mishra (admin@brightprogrammer.in)
  * @copyright Copyright (c) 2024 Siddharth Mishra
  * @copyright Copyright (c) 2024 Anvie Labs
  *
- * @brief Some Vulkan helper methods to create surface and other things.
+ * Copyright 2024 Siddharth Mishra, Anvie Labs
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted 
+ * provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the
+ *    distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+ *    or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * */
 
 #ifndef CROSSWINDOW_VULKAN_H
 #define CROSSWINDOW_VULKAN_H
 
-#include <Types.h>
 #include <CrossWindow/Window.h>
+#include <Types.h>
 #include <vulkan/vulkan.h>
 
 /* vulkan specific methods */
